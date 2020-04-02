396a964b63ae31e989ac975c44f1a73682eb9ece
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2020-04-02 16:07:55 +0100

    added logging and overhauled, streamlined script, added functionality for shortlisted abstracts

--- compile_papers.py
+++ compile_papers.py
@@ -1,111 +1,188 @@
 from glob import iglob
 from pathlib import Path
 import csv
-import shutil
-from os import chdir
+from shutil import move, copy
+import logging
+import sys
 
-# make this input maybe? input('paste location of csv shortlists: ')
+# input('paste location of csv shortlists: ')
 csv_path = r'T:\Ascential Events\WARC\Public\WARC.com\Editorial\Awards (Warc)\2020 Awards\1. WARC Awards\Scoresheets back from judges\Shortlists'
 csv_files = iglob(fr'{csv_path}\\*.csv')
+# input('paste destination location: ')
 path = r'T:\Ascential Events\WARC\Backup Server\Loading\Monthly content for Newgen\Project content - March 2020\WARC Awards 2020'
 
-def each_id():
+def setup_custom_logger(name):
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%d-%m-%Y %H:%M:%S')
+    handler = logging.FileHandler(name, mode='w')
+    handler.setFormatter(formatter)
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    logger.addHandler(screen_handler)
+    return logger
 
-	for f in csv_files:
-		# this will become the directory name
-		dn = f.split('\\')[-1].split('_')[0].replace('.csv', '')
-		print(dn)
+logger = setup_custom_logger('logs/compile-papers-log.txt')
 
-		# creates new directories if they don't exist
-		newdir = Path.cwd() / fr'Shortlisted papers\\edited docs\\{dn}'
-		if not Path(newdir).is_dir():
-			newdir.mkdir(parents=True, exist_ok=True)
+def file_copier(files, dst):
 
-		with open(f, newline='') as csvf:
-			r = csv.DictReader(csvf)
-			for row in r:
-				# catch blank rows 
-				if len(row['ID']) > 3:
-					ID = row['ID']
-					# should this be indented?
-					# this is weak because of folder naming
-					docx_files = iglob(fr'{path}\\Returned papers & abstracts\\**\\{ID}*.docx', recursive=True)
-					for file in docx_files:
-						fn = file.split('\\')[-1]
-						if not 'MARKUP' in fn:
+	for file in files:
+		fn = file.split('\\')[-1]
+		try:
+			copy(file, f'{dst}\\{fn}')
+			logger.info(fn)
+		except Exception as e:
+			logger.error(e)
+			continue
 
-							try:
-								shutil.copy(file, f'Shortlisted papers\\edited docs\\{dn}\\{fn}')
-								print(fn)
-							except Exception as e:
-								print(e)
+def file_mover(files, dst):
 
-def each_ref():
-
-	for f in csv_files:
-		# this will become the directory name
-		dn = f.split('\\')[-1].split('_')[0].replace('.csv', '')
-		print(dn)
-
-		# creates new directories if they don't exist
-		newdir = Path.cwd() / fr'Shortlisted papers\\pdfs\\{dn}'
-		if not Path(newdir).is_dir():
-			newdir.mkdir(parents=True, exist_ok=True)
-
-		with open(f, newline='') as csvf:
-			r = csv.DictReader(csvf)
-			for row in r:
-				# catch blank rows 
-				if len(row['ID']) > 3:
-					ID = row['ID']
-					# should this be indented?
-					# this is weak because of folder naming
-					pdf_files = iglob(fr'{path}\\Judges papers\\**\\*{ID}*.pdf', recursive=True)
-					for file in pdf_files:
-						fn = file.split('\\')[-1]
-
-						try:
-							shutil.copy(file, f'Shortlisted papers\\pdfs\\{dn}\\{fn}')
-							print(fn)
-						except Exception as e:
-								print(e)
-				
+	for file in files:
+		fn = file.split('\\')[-1]
+		try:
+			move(file, f'{dst}\\{fn}')
+			logger.info(fn)
+		except Exception as e:
+			logger.error(e)
+			continue
 
 def main():
-	
-	print(f"Original directory: '{Path.cwd()}'")
-
-	try:
-	    # change directory to the path inputed
-	    chdir(path)
-	    print(fr'Changed directory')
-
-	except Exception as e:
-	    # print exceptions
-	    print(f"Couldn't access path: '{path}'\n ensure you've pasted a valid folder path") # print, not log
-	    print(e)
-
-	print(f"Current directory: '{Path.cwd()}'")
+	'''moves or copies assets for shortlisted entries only'''
 
 	select = int(input('''
 
 		- '1' for docx 
 		- '2' for pdfs
+		- '3' for abstracts
 		
 		> select: '''))
 
-	if select == 1:
-		each_id()
-	elif select == 2:
-		each_ref()
-	else:
+	if select not in range(1,4):
+		print('\n\t- incorrect input')
 		main()
+	else:
 
+		# check_dirs() # works
+		logger.info(f"checking dirs exist in '{path}'...")
+		for f in csv_files:
+			# this will become the new directory name
+			dn = f.split('\\')[-1].split('_')[0].replace('.csv', '')
 
+			# destination directories
+			# SHORTLISTS
+			docx_dst = path + fr'\\edited papers\\shortlisted\\docs\\{dn}'
+			pdf_dst = path + fr'\\edited papers\\shortlisted\\pdfs\\{dn}'
+			abs_dst = path + fr'\\abstracts\\shortlisted\\{dn}'
+			# ENTRANTS
+			ent_docx_dst = path + fr'\\edited papers\\entrants\\{dn}'
+			ent_abs_dst = path + fr'\\abstracts\\entrants\\{dn}'
+			# MURKIES
+			mrk_docx_dst = path + fr'\\edited papers\\murkies\\{dn}'
+			mrk_abs_dst = path + fr'\\abstracts\\murkies\\{dn}'
 
-	# print(each_ref()) # why doesn't this work?
-	# pprint()
-	# pprint(pdf_shortlist())
+			logger.info(f'### {dn} ###')
+			for dst in [docx_dst, pdf_dst, abs_dst, ent_docx_dst, ent_abs_dst, mrk_abs_dst, mrk_docx_dst]:
+				dst = Path(dst)
+				if not dst.is_dir():
+					dst.mkdir(parents=True, exist_ok=True)
+					print('made dir:', str(dst).split('\\')[-3:])
+			logger.info('dir check finished')
+
+			try:
+				with open(f, newline='') as csvf:
+					r = csv.DictReader(csvf)
+					for row in r:
+						# catch blank rows 
+						if len(row['ID']) > 3:
+							ID = row['ID']
+							docx_files = iglob(fr'{path}\\Returned papers & abstracts\\**\\{ID}*.docx', recursive=True)
+							pdf_files = iglob(fr'{path}\\Judges papers\\**\\*{ID}*.pdf', recursive=True)
+							txt_files = iglob(fr'Abstracts cleanup\\abstracts\\{ID}*.txt', recursive=True)
+
+							if select == 1:
+								pass
+								file_copier(docx_files, docx_dst)
+							elif select == 2:
+								pass
+								file_copier(pdf_files, pdf_dst)
+							elif select == 3:
+								file_mover(txt_files, abs_dst)
+
+			except Exception as e:
+				logger.error(e)
+				logger.info('no csv files in path')
 
 if __name__ == '__main__':
 	main()
+
+
+## OLD CODE ##
+
+# def shortlisted_docxs():
+
+# 	for f in csv_files:
+# 		# this will become the new directory name
+# 		dn = f.split('\\')[-1].split('_')[0].replace('.csv', '')
+# 		print(dn)
+
+# 		# creates new directories if they don't exist
+# 		docx_dst = path / fr'Shortlisted papers\\edited docs\\{dn}'
+
+# 		if not Path(docx_dst).is_dir():
+# 			docx_dst.mkdir(parents=True, exist_ok=True)
+
+# 		with open(f, newline='') as csvf:
+# 			r = csv.DictReader(csvf)
+# 			for row in r:
+# 				# catch blank rows 
+# 				if len(row['ID']) > 3:
+# 					ID = row['ID']
+# 					# this is weak because of folder naming
+# 					docx_files = iglob(fr'{path}\\Returned papers & abstracts\\**\\{ID}*.docx', recursive=True)
+# 					for file in docx_files:
+# 						p = Path(file)
+# 						fn = p.parts[-1]
+
+# 						if not 'MARKUP' in fn:
+
+# 							try:
+# 								copy(file, f'{docx_dst}\\{fn}')
+# 								print(fn)
+# 							except Exception as e:
+# 								logger.error(e)
+# 								continue
+
+# def shortlisted_pdfs():
+
+# 	for f in csv_files:
+# 		# this will become the new directory name
+# 		dn = f.split('\\')[-1].split('_')[0].replace('.csv', '')
+# 		print(dn)
+
+# 		# creates new directories for pdfs if they don't exist
+# 		pdf_dst = path / fr'Shortlisted papers\\pdfs\\{dn}'
+
+# 		if not Path(pdf_dst).is_dir():
+# 			pdf_dst.mkdir(parents=True, exist_ok=True)
+
+# 		with open(f, newline='') as csvf:
+# 			r = csv.DictReader(csvf)
+# 			for row in r:
+# 				# catch blank rows 
+# 				if len(row['ID']) > 3:
+# 					ID = row['ID']
+# 					# this is weak because of folder naming
+# 					pdf_files = iglob(fr'{path}\\Judges papers\\**\\*{ID}*.pdf', recursive=True)
+
+# 					for file in pdf_files:
+# 						p = Path(file)
+# 						fn = p.parts[-1]
+
+# 						try:
+# 							copy(file, f'{pdf_dst}\\{fn}')
+# 							print(fn)
+# 						except Exception as e:
+# 							logger.error(e)
+# 							continue
